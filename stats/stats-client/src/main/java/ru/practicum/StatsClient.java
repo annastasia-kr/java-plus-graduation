74adbd62ba6a15a41363af2285ba2e1ea7832b65
avkra <pr.anastasia.k@yandex.ru>
2026-03-25 19:02:56 +0300

    #5: правки StatsClient.java

--- stats/stats-client/src/main/java/ru/practicum/StatsClient.java
+++ stats/stats-client/src/main/java/ru/practicum/StatsClient.java
@@ -1,14 +1,19 @@
 package ru.practicum;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @FeignClient(name = "stats-server")
 public interface StatsClient {
+
+    @PostMapping("/hit")
     void saveHit(jakarta.servlet.http.HttpServletRequest request);
 
+    @GetMapping("/stats")
     List<StatsDto> getStats(LocalDateTime start, LocalDateTime end, List<String> uris, Boolean unique);
 
 }
