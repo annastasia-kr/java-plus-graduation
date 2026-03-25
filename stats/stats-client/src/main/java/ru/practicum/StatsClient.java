e8acd6e3d3e2198e6d7c3d3be611d52e70d83a20
avkra <pr.anastasia.k@yandex.ru>
2026-03-25 17:48:47 +0300

    #4: изменение application.yaml

--- stats/stats-client/src/main/java/ru/practicum/StatsClient.java
+++ stats/stats-client/src/main/java/ru/practicum/StatsClient.java
@@ -1,8 +1,11 @@
 package ru.practicum;
 
+import org.springframework.cloud.openfeign.FeignClient;
+
 import java.time.LocalDateTime;
 import java.util.List;
 
+@FeignClient(name = "stats-server")
 public interface StatsClient {
     void saveHit(jakarta.servlet.http.HttpServletRequest request);
 
