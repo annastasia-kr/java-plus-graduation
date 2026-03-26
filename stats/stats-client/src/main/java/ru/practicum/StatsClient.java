dcaac2dc1a7010b62ff94bfa837d8b914c359e65
avkra <pr.anastasia.k@yandex.ru>
2026-03-26 19:50:56 +0300

    #14:правки stat-service

--- stats/stats-client/src/main/java/ru/practicum/StatsClient.java
+++ stats/stats-client/src/main/java/ru/practicum/StatsClient.java
@@ -1,25 +1,11 @@
 package ru.practicum;
 
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-
 import java.time.LocalDateTime;
 import java.util.List;
 
-@FeignClient(name = "stats-server")
 public interface StatsClient {
+    void saveHit(jakarta.servlet.http.HttpServletRequest request);
 
-    @PostMapping("/hit")
-    void saveHit(@RequestBody jakarta.servlet.http.HttpServletRequest request);
-
-    @GetMapping("/stats")
-    List<StatsDto> getStats(@RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime start,
-                            @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime end,
-                            @RequestParam(required = false) List<String> uris,
-                            @RequestParam(required = false) Boolean unique);
+    List<StatsDto> getStats(LocalDateTime start, LocalDateTime end, List<String> uris, Boolean unique);
 
 }
