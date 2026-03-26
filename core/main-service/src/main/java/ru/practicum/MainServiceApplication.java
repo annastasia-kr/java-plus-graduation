dabe2a701d83927f5e1066044103319618592e36
avkra <pr.anastasia.k@yandex.ru>
2026-03-26 20:46:24 +0300

    #14:правки pom

--- core/main-service/src/main/java/ru/practicum/MainServiceApplication.java
+++ core/main-service/src/main/java/ru/practicum/MainServiceApplication.java
@@ -3,11 +3,9 @@ package ru.practicum;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients(clients = StatsClient.class)
 public class MainServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(MainServiceApplication.class, args);
