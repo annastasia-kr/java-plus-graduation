cce59d9ab51512756cac04249c0162391edc9b94
avkra <pr.anastasia.k@yandex.ru>
2026-03-31 11:36:12 +0300

    #3: исправление ошибки feign клиента

--- core/interaction-api/src/main/java/ru/practicum/event/client/EventClient.java
+++ core/interaction-api/src/main/java/ru/practicum/event/client/EventClient.java
@@ -2,6 +2,6 @@ package ru.practicum.event.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient(name = "event-service", fallback = EventClient.class)
+@FeignClient(name = "event-service", fallback = EventClientFallback.class)
 public interface EventClient extends EventOperations {
 }
