ab0d64d0a2b1a049361c91d36c0e6e765fc361ef
avkra <pr.anastasia.k@yandex.ru>
2026-04-01 11:22:54 +0300

    #13: проверка

--- core/request-service/src/main/java/ru/practicum/RequestServiceApplication.java
+++ core/request-service/src/main/java/ru/practicum/RequestServiceApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import ru.practicum.request.client.RequestClient;
+import ru.practicum.event.client.EventClient;
+import ru.practicum.user.client.UserClient;
 
 @SpringBootApplication
-@EnableFeignClients(clients = RequestClient.class)
+@EnableFeignClients(clients = {UserClient.class, EventClient.class})
 @EnableDiscoveryClient
 public class RequestServiceApplication {
     public static void main(String[] args) {
--- stats/stats-client/src/main/java/ru/practicum/client/impl/StatsClient.java
+++ stats/stats-client/src/main/java/ru/practicum/client/impl/StatsClient.java
@@ -1,10 +1,12 @@
 package ru.practicum.client.impl;
 
+import org.springframework.cloud.openfeign.FeignClient;
 import ru.practicum.StatsDto;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+@FeignClient(name = "stats-server")
 public interface StatsClient {
     void saveHit(jakarta.servlet.http.HttpServletRequest request);
 
