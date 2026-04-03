fab794b89be7f1702fbe8647572e1db3ebd576ed
avkra <pr.anastasia.k@yandex.ru>
2026-04-03 08:48:04 +0300

    fix

--- core/interaction-api/src/main/java/ru/practicum/event/dto/EventResult.java
+++ core/interaction-api/src/main/java/ru/practicum/event/dto/EventResult.java
@@ -1,11 +1,5 @@
 package ru.practicum.event.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
-@AllArgsConstructor
 public record EventResult(Long eventId, Long count) {
 }
