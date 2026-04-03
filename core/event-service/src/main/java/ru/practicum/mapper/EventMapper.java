2eae7ba29c50055020482ff99cbcff2cc0c0ec0d
avkra <pr.anastasia.k@yandex.ru>
2026-04-03 16:11:23 +0300

    Correct event mapper

--- core/event-service/src/main/java/ru/practicum/mapper/EventMapper.java
+++ core/event-service/src/main/java/ru/practicum/mapper/EventMapper.java
@@ -38,7 +38,7 @@ public interface EventMapper {
     @Mapping(source = "confirmedRequests", target = "confirmedRequests")
     @Mapping(source = "views", target = "views")
     @Mapping(source = "event.initiator", target = "initiator")
-    @Mapping(source = "participantLimit", target = "participantLimit")
+    @Mapping(source = "event.participantLimit", target = "participantLimit")
     EventDto toEventDto(Event event, Long confirmedRequests, Long views);
 
     @Mapping(source = "category", target = "category")
