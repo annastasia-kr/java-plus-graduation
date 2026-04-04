7023668ad3f07b79405a39692f487da7d26a448a
avkra <pr.anastasia.k@yandex.ru>
2026-04-04 21:44:32 +0300

    test

--- stats/stats-server/src/main/java/ru/practicum/exception/handler/ErrorHandler.java
+++ stats/stats-server/src/main/java/ru/practicum/exception/handler/ErrorHandler.java
@@ -2,6 +2,7 @@ package ru.practicum.exception.handler;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -16,6 +17,13 @@ public class ErrorHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleValidationException(final ValidationException e) {
         log.warn("Validation failed: {}", e.getMessage());
+        return new ErrorResponse("BAD_REQUEST");
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleMissingParam(MissingServletRequestParameterException e) {
+        log.error("Missing required parameter: {}", e.getParameterName());
         return new ErrorResponse("BAD_REQUEST");
     }
 
