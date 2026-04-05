2d0bb16fb9675341f3ccb35e05ab70fc145b5115
avkra <pr.anastasia.k@yandex.ru>
2026-04-05 17:05:37 +0300

    test

--- stats/stats-server/src/main/java/ru/practicum/controller/StatsController.java
+++ stats/stats-server/src/main/java/ru/practicum/controller/StatsController.java
@@ -6,7 +6,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.HitDto;
 import ru.practicum.StatsDto;
@@ -19,7 +18,6 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @Slf4j
-@Validated
 public class StatsController {
 
     private final StatsService statsService;
--- stats/stats-server/src/main/java/ru/practicum/model/Hit.java
+++ stats/stats-server/src/main/java/ru/practicum/model/Hit.java
@@ -14,6 +14,7 @@ import java.util.Objects;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@ToString
 @Table(name = "hits")
 public class Hit {
 
