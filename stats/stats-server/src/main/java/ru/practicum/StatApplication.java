18dd21c2b826a014a8291b50009ae836236ab6ef
avkra <pr.anastasia.k@yandex.ru>
2026-04-05 17:43:21 +0300

    test

--- stats/stats-server/src/main/java/ru/practicum/StatApplication.java
+++ stats/stats-server/src/main/java/ru/practicum/StatApplication.java
@@ -2,10 +2,10 @@ package ru.practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableFeignClients(basePackages = "ru.practicum")
 public class StatApplication {
     public static void main(String[] args) {
         SpringApplication.run(StatApplication.class, args);
